e71018f190e573181340ea6dfd7e3535748408f4
asjad <asjad.ajic@mail.com>
2025-03-18 04:34:09 +0530

    Add: Model

--- gender_recognition/utils.py
+++ gender_recognition/utils.py
@@ -6,8 +6,6 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MODEL_PATH = os.path.join(BASE_DIR, "gender_model.h5")
 
-
-
 def extract_features(file_path):
     y, sr = librosa.load(file_path, sr=22050)
     mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=20)  # Extract 20 MFCCs
